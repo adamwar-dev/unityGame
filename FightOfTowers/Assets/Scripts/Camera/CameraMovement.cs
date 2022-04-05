5874ce858d8aac8b95bf040e9ac14739e58a330d
tornal-dev <78816725+tornal-dev@users.noreply.github.com>
2022-04-05 21:45:34 +0200

    Parallax Background and textures

--- FightOfTowers/Assets/Scripts/Camera/CameraMovement.cs
+++ FightOfTowers/Assets/Scripts/Camera/CameraMovement.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 */
 public class CameraMovement : MonoBehaviour
 {
-	[SerializeField] GameObject ground;
+	[SerializeField] float start;
+	[SerializeField] float end;
 	[SerializeField] float moveCameraSpeed = 5;
 	[SerializeField] float moveCameraSpeedFast = 10;
 
@@ -17,21 +18,18 @@ public class CameraMovement : MonoBehaviour
 
 	void MoveCamera() {
 		float cameraHalfWidth = Camera.main.orthographicSize * Camera.main.aspect;
-		float groundWidth = ground.transform.localScale.x;
-		float mapStart = ground.transform.position.x - groundWidth/2;
-		float mapEnd = mapStart + groundWidth - transform.localScale.x/2;
 
 		if (Input.mousePosition.y < Screen.height * 0.8) {
-			if (Input.mousePosition.x > Screen.width * 0.9 && Input.mousePosition.x < Screen.width * 0.97 && transform.position.x < mapEnd) {
+			if (Input.mousePosition.x > Screen.width * 0.9 && Input.mousePosition.x < Screen.width * 0.97 && transform.position.x < end - transform.localScale.x/2) {
 				transform.position = new Vector3(transform.position.x + moveCameraSpeed * Time.deltaTime, transform.position.y, transform.position.z);
 			} 
-			else if (Input.mousePosition.x >= Screen.width * 0.97 && transform.position.x < mapEnd) {
+			else if (Input.mousePosition.x >= Screen.width * 0.97 && transform.position.x < end - transform.localScale.x/2) {
 				transform.position = new Vector3(transform.position.x + moveCameraSpeedFast * Time.deltaTime, transform.position.y, transform.position.z);
 			}
-			else if (Input.mousePosition.x < Screen.width * 0.1 && Input.mousePosition.x > Screen.width * 0.03 && transform.position.x > mapStart + transform.localScale.x/2) {
+			else if (Input.mousePosition.x < Screen.width * 0.1 && Input.mousePosition.x > Screen.width * 0.03 && transform.position.x > start + transform.localScale.x/2) {
 				transform.position = new Vector3(transform.position.x - moveCameraSpeed * Time.deltaTime, transform.position.y, transform.position.z);
 			}
-			else if (Input.mousePosition.x <= Screen.width * 0.03 && transform.position.x > mapStart + transform.localScale.x/2) {
+			else if (Input.mousePosition.x <= Screen.width * 0.03 && transform.position.x > start + transform.localScale.x/2) {
 				transform.position = new Vector3(transform.position.x - moveCameraSpeedFast * Time.deltaTime, transform.position.y, transform.position.z);
 			}
 		}
