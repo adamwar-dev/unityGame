38fab6c910fcd73376135b01ce399898662d7076
MikolajM21 <mikolaj.malich@gmail.com>
2022-04-05 13:27:04 +0200

    Added healthbars with behaviour scripts

--- FightOfTowers/Assets/Bases/BaseScripts/Player.cs
+++ FightOfTowers/Assets/Bases/BaseScripts/Player.cs
@@ -1,18 +1,33 @@
+/* author: Mikolaj Malich */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentHealth >= 10)
+        {
+            //testowa dekrementacja healthbar'a przy wcisnieciu spacji
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                currentHealth -= 10;
+                healthBar.SetHealth(currentHealth);
+            }
+        }
     }
 }
